8133dc75f2667496e57b9c1d32d5f1f46bc947aa
FooQoo <horseshoes.buzz.17@gmail.com>
2021-06-22 23:19:44 +0900

    add spring project

---
+++ src/main/java/covid/fukui/rss/articlefeeder/ArticleFeederApplication.java
@@ -0,0 +1,12 @@
+package covid.fukui.rss.articlefeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleFeederApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ArticleFeederApplication.class, args);
+    }
+}
---
+++ src/test/java/covid/fukui/rss/articlefeeder/ArticleFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package covid.fukui.rss.articlefeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
